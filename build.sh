93d5c33fc0a1ac84baa0987796ed5d66e093733a
Philip Waritschlager <philip@waritschlager.de>
2025-09-12 23:38:34 +0200

    .

--- build.sh
+++ build.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
+
 set -eo pipefail
 
-source_file="milk.mmd"
-trans_file="milk._trans.mmd"
-build_file="milk._build.mmd"
-
-# en###de. aicodedont@me
-perl -0777 -pe '
-	s/\["([^"\\]*?(?:\\.[^"\\]*?)*?)###([^"\\]*?(?:\\.[^"\\]*?)*?)"\]/["$1<br\/><span class=\"de_translation\">$2<\/span>"]/g;
-	s/--\s*([^\-\n>]*?)###([^\-\n>]*?)\s*-->/-- $1<br\/><span class=\"de_translation\">$2<\/span> -->/g;
-' "$source_file" > "$trans_file"
-
-# {{{filename}}}
-box_size=100
-awk -v S="$box_size" 'function repl(fname){base="https://commons.wikimedia.org/wiki/Special:FilePath"; if(fname ~ /Cr%C3%A8me|Milk-bottle/) base="https://en.wikipedia.org/wiki/Special:FilePath"; return "<div class=\"wikimedia_box\"><img class=\"wikimedia_image\" src=\"" base "/" fname "?width=" S "\"\/><\/div><br>"} { while(match($0,/\{\{\{([^}]+)\}\}\}/,m)){ $0 = substr($0,1,RSTART-1) repl(m[1]) substr($0,RSTART+RLENGTH) } print }' "$trans_file" > "$build_file"
+cat milk.mmd \
+	| sed -E 's/IMG:::(.+?):([0-9]+):([0-9]+)/<a href='"'"'https:\/\/commons.wikimedia.org\/wiki\/File:\1'"'"'><img src='"'"'https:\/\/commons.wikimedia.org\/wiki\/Special:FilePath\/\1?width=\2'"'"' width='"'"'\2'"'"' height='"'"'\3'"'"'><\/a>/g' \
+	| sed -E 's/###(.+?)"/<br\/><span class=\"de\">\1<\/span>"/g' \
+	> tmp.mmd
 
 docker run --rm -u root -v "$PWD":/data minlag/mermaid-cli \
-	-i /data/"$build_file" \
+	-i /data/tmp.mmd \
 	-o /data/milk.mmd.svg \
 	-b transparent \
 	-c /data/mermaid_config.json \
 	--cssFile /data/milk.css \
 	--quiet
+rm tmp.mmd
 
-rm -f "$build_file" "$trans_file"
+sed -i -E 's/width: 100%;//g' milk.mmd.svg
