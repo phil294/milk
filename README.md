923fd03617319600c1b65fe601f8173607087a76
Philip Waritschlager <philip@waritschlager.de>
2025-09-13 16:08:40 +0200

    .

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ http://waritschlager.de/milk.html
 
 Run `./build.sh`, then open `milk.html` in your browser
 
+## Layout
+
+The layout can be changed primarily by changing the order of the DEFINITIONS in `milk.mmd`. This is very fragile and can result in a complete mess just by moving any single line. Because of this, the mermaid (cli/docker) version is fixed in place in `build.sh`, but there may be room for improvement.
+
 ## License
 
 AGPL
--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 set -eo pipefail
 
 cat milk.mmd \
+	| perl -pe 's/WIKI:::(.+?):(.+?)(?=["#<\/,])/<a href='"'"'https:\/\/en.wikipedia.org\/wiki\/\1'"'"'>\2<\/a>/g' \
+	| perl -pe 's/WIKI:::(.+?)(?=["#<\/])/<a href='"'"'https:\/\/en.wikipedia.org\/wiki\/\1'"'"'>\1<\/a>/g' \
 	| sed -E 's/IMG:::(.+?):([0-9]+):([0-9]+)/<a href='"'"'https:\/\/commons.wikimedia.org\/wiki\/File:\1'"'"'><img src='"'"'https:\/\/commons.wikimedia.org\/wiki\/Special:FilePath\/\1?width=\2'"'"' width='"'"'\2'"'"' height='"'"'\3'"'"'><\/a>/g' \
-	| sed -E 's/###(.+?)"/<br\/><span class=\"de\">\1<\/span>"/g' \
+	| sed -E 's/###(.+?)("|-->)/<br\/><span class=\"de\">\1<\/span>\2/g' \
 	> tmp.mmd
 
-docker run --rm -u root -v "$PWD":/data minlag/mermaid-cli \
+docker run --rm -u root -v "$PWD":/data minlag/mermaid-cli:11.9.1-beta.3 \
 	-i /data/tmp.mmd \
 	-o /data/milk.mmd.svg \
 	-b transparent \
